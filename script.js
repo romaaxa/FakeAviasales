44819a3463ad3512ad6ca4b3c396d2447babf712
Anton <a.romashko.work@mail.ru>
2020-03-19 16:20:56 +0300

    dynamic search (to what place flying)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const formSearch = document.querySelector('.form-search'), //search
-    inputCitiesFrom = document.querySelector('.input__cities-from'),
-    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'), //list
-    inputCitiesTo = document.querySelector('.input__cities-to'),
-    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
-    inputDateDepart = document.querySelector('.input__date-depart');
+    inputCitiesFrom = document.querySelector('.input__cities-from'), //from
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'), //list from
+    inputCitiesTo = document.querySelector('.input__cities-to'),  //to
+    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'), //list to
+    inputDateDepart = document.querySelector('.input__date-depart'); //date
 
 const city = ['Minsk', 'Grodno', 'Warshaw', 'Moscow', 'London'];
 
@@ -37,5 +37,38 @@ dropdownCitiesFrom.addEventListener('click', (event) => {
     if (target.tagName.toLowerCase() === 'li') {
         inputCitiesFrom.value = target.textContent;
         dropdownCitiesFrom.textContent = '';
+    }
+});
+
+const showCityTo = (input, list) => {
+    list.textContent = ''; //clear input string every new symbol
+
+    if (inputCitiesTo.value === '') { return; } //unnecessary
+    if (input.value !== '') {
+        const cityFilter = city.filter((item) => {
+            const fixItem = item.toLowerCase();
+            return fixItem.includes(input.value.toLowerCase());
+        });
+
+        cityFilter.forEach((item) => {
+            const lielements = document.createElement('li'); //add element in dynamic searching
+
+            lielements.classList.add('dropdown__city');
+            lielements.textContent = item;
+
+            list.append(lielements); //displaying
+        });
+    }
+};
+
+inputCitiesTo.addEventListener('input', () => {
+    showCityTo(inputCitiesTo, dropdownCitiesTo)
+});
+
+dropdownCitiesTo.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName.toLowerCase() === 'li') {
+        inputCitiesTo.value = target.textContent;
+        dropdownCitiesTo.textContent = '';
     }
 });
