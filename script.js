78717cab858f88ac0e615eefbfedad046d518566
Anton <a.romashko.work@mail.ru>
2020-03-20 10:24:27 +0300

    fixed -to- dynamic input

--- script.js
+++ script.js
@@ -1,74 +1,55 @@
-const formSearch = document.querySelector('.form-search'), //search
-    inputCitiesFrom = document.querySelector('.input__cities-from'), //from
-    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'), //list from
-    inputCitiesTo = document.querySelector('.input__cities-to'),  //to
-    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'), //list to
-    inputDateDepart = document.querySelector('.input__date-depart'); //date
+const formSearch = document.querySelector(".form-search"), //search
+  inputCitiesFrom = document.querySelector(".input__cities-from"), //from
+  dropdownCitiesFrom = document.querySelector(".dropdown__cities-from"), //list from
+  inputCitiesTo = document.querySelector(".input__cities-to"), //to
+  dropdownCitiesTo = document.querySelector(".dropdown__cities-to"), //list to
+  inputDateDepart = document.querySelector(".input__date-depart"); //date
 
-const city = ['Minsk', 'Grodno', 'Warshaw', 'Moscow', 'London'];
+const city = ["Minsk", "Grodno", "Warshaw", "Moscow", "London"];
 
 const showCity = (input, list) => {
-    list.textContent = ''; //clear input string every new symbol
+  list.textContent = ""; //clear input string every new symbol
 
-    if (inputCitiesFrom.value === '') { return; } //unnecessary
-    if (input.value !== '') {
-        const cityFilter = city.filter((item) => {
-            const fixItem = item.toLowerCase();
-            return fixItem.includes(input.value.toLowerCase());
-        });
+  if (inputCitiesFrom.value === "") {
+    return;
+  } //unnecessary
+  if (input.value !== "") {
+    const cityFilter = city.filter(item => {
+      const fixItem = item.toLowerCase();
+      return fixItem.includes(input.value.toLowerCase());
+    });
 
-        cityFilter.forEach((item) => {
-            const lielements = document.createElement('li'); //add element in dynamic searching
+    cityFilter.forEach(item => {
+      const lielements = document.createElement("li"); //add element in dynamic searching
 
-            lielements.classList.add('dropdown__city');
-            lielements.textContent = item;
+      lielements.classList.add("dropdown__city");
+      lielements.textContent = item;
 
-            list.append(lielements); //displaying
-        });
-    }
+      list.append(lielements); //displaying
+    });
+  }
 };
 
-inputCitiesFrom.addEventListener('input', () => {
-    showCity(inputCitiesFrom, dropdownCitiesFrom)
+inputCitiesFrom.addEventListener("input", () => {
+  showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 
-dropdownCitiesFrom.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.tagName.toLowerCase() === 'li') {
-        inputCitiesFrom.value = target.textContent;
-        dropdownCitiesFrom.textContent = '';
-    }
+dropdownCitiesFrom.addEventListener("click", event => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === "li") {
+    inputCitiesFrom.value = target.textContent;
+    dropdownCitiesFrom.textContent = "";
+  }
 });
 
-const showCityTo = (input, list) => {
-    list.textContent = ''; //clear input string every new symbol
-
-    if (inputCitiesTo.value === '') { return; } //unnecessary
-    if (input.value !== '') {
-        const cityFilter = city.filter((item) => {
-            const fixItem = item.toLowerCase();
-            return fixItem.includes(input.value.toLowerCase());
-        });
-
-        cityFilter.forEach((item) => {
-            const lielements = document.createElement('li'); //add element in dynamic searching
-
-            lielements.classList.add('dropdown__city');
-            lielements.textContent = item;
-
-            list.append(lielements); //displaying
-        });
-    }
-};
-
-inputCitiesTo.addEventListener('input', () => {
-    showCityTo(inputCitiesTo, dropdownCitiesTo)
+inputCitiesTo.addEventListener("input", () => {
+  showCity(inputCitiesTo, dropdownCitiesTo);
 });
 
-dropdownCitiesTo.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.tagName.toLowerCase() === 'li') {
-        inputCitiesTo.value = target.textContent;
-        dropdownCitiesTo.textContent = '';
-    }
+dropdownCitiesTo.addEventListener("click", event => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === "li") {
+    inputCitiesTo.value = target.textContent;
+    dropdownCitiesTo.textContent = "";
+  }
 });
